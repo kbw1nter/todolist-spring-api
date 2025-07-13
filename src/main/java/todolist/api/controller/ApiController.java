81e9d73c638ab32f1f5f3be39505c98aa9b8140f
kanan <kanandabw@gmail.com>
2025-07-13 18:20:22 -0300

    first commit

---
+++ src/main/java/todolist/api/controller/ApiController.java
@@ -0,0 +1,34 @@
+package todolist.api.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class ApiController {
+
+    private List<String> tasks = new ArrayList<>();
+    private ObjectMapper objectMapper;
+
+    public ApiController(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @GetMapping(path = "/tasks")
+    public ResponseEntity<String> helloWorld() throws JsonProcessingException {
+        tasks.add("My task");
+        return ResponseEntity.ok(objectMapper.writeValueAsString(tasks));
+    }
+
+    @PostMapping(path = "/tasks")
+    public  ResponseEntity<Void> createTask(@RequestBody String task){
+        tasks.add(task);
+        return ResponseEntity.ok().build();
+    }
+}
