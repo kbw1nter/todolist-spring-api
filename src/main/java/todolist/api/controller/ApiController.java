00b68ca675a2f34f9f594d91006948b08af365ad
kanan <kanandabw@gmail.com>
2025-07-14 10:48:18 -0300

    update

--- src/main/java/todolist/api/controller/ApiController.java
+++ src/main/java/todolist/api/controller/ApiController.java
@@ -3,32 +3,36 @@ package todolist.api.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping(path = "/tasks")
 public class ApiController {
 
     private List<String> tasks = new ArrayList<>();
-    private ObjectMapper objectMapper;
+    private final ObjectMapper objectMapper;
 
     public ApiController(ObjectMapper objectMapper) {
         this.objectMapper = objectMapper;
     }
 
-    @GetMapping(path = "/tasks")
-    public ResponseEntity<String> helloWorld() throws JsonProcessingException {
-        tasks.add("My task");
+    @GetMapping
+    public ResponseEntity<String> listTasks() throws JsonProcessingException {
         return ResponseEntity.ok(objectMapper.writeValueAsString(tasks));
     }
 
-    @PostMapping(path = "/tasks")
+    @PostMapping
     public  ResponseEntity<Void> createTask(@RequestBody String task){
         tasks.add(task);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteTasks(){
+        tasks = new ArrayList<>();
         return ResponseEntity.ok().build();
     }
 }
